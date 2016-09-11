85a76132c68d4ac890500f0d5bbe2aee300a7abc
druidream <druidream@gmail.com>
2016-09-11 17:30:33 +0800

    added icons

--- Bibliomania/BMDrawerViewController.m
+++ Bibliomania/BMDrawerViewController.m
@@ -8,30 +8,54 @@
 
 #import "BMDrawerViewController.h"
 
-@interface BMDrawerViewController ()
+@interface BMDrawerViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *drawerItems;
 
 @end
 
 @implementation BMDrawerViewController
 
+static NSString * const CELL_IDENTIFIER = @"drawerItemCell";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.drawerItems = [[NSMutableArray alloc] init];
+    [self.drawerItems addObject:@"Dashboard"];
+    [self.drawerItems addObject:@"Books"];
+    [self.drawerItems addObject:@"Tags"];
+    [self.drawerItems addObject:@"Wishlist"];
+    [self.drawerItems addObject:@"Settings"];
+
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UITableViewDataSource, UITableViewDelegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_IDENTIFIER];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CELL_IDENTIFIER];
+    }
+    cell.backgroundColor = [UIColor clearColor];
+    NSString *labelName = [self.drawerItems objectAtIndex:indexPath.row];
+    cell.textLabel.text = labelName;
+    NSString *imageName = [NSString stringWithFormat:@"drawer-%@", labelName.lowercaseString];
+    cell.imageView.image = [UIImage imageNamed:imageName];
+    
+    return cell;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.drawerItems.count;
 }
-*/
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 70;
+}
 
 @end
